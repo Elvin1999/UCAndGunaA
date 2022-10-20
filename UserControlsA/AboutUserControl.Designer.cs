df5d5d8d32516e1f04f2b4e6432b3fc228318699
Elvin <elvincamalzade00@gmail.com>
2022-10-20 10:52:49 +0400

    Add project files.

---
+++ UserControlsA/AboutUserControl.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace UserControlsA
+{
+    partial class AboutUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(110, 126);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "About Page";
+            // 
+            // AboutUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "AboutUserControl";
+            this.Size = new System.Drawing.Size(980, 450);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ UserControlsA/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControlsA
+{
+    public partial class Form1 : Form
+    {
+        public HomeUserControl HomeUC { get; set; }
+        public AboutUserControl AboutUC { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            LoadHomeUC();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove(HomeUC);
+
+            AboutUC = new AboutUserControl();
+            AboutUC.Location = new Point(0, 100);
+
+            this.Controls.Add(AboutUC);
+
+
+        }
+        private void LoadHomeUC()
+        {
+            HomeUC = new HomeUserControl();
+            HomeUC.Location = new Point(0, 100);
+
+            this.Controls.Add(HomeUC);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove(AboutUC);
+            LoadHomeUC();
+        }
+    }
+}
